f207eed62a674015acb3a8215e74da76a243c970
William Reed <wreed58@gmail.com>
2019-05-04 15:56:38 -0400

    multi key support - generate bundles easier

--- src/main/kotlin/dev/williamreed/signal/Client.kt
+++ src/main/kotlin/dev/williamreed/signal/Client.kt
@@ -1,43 +1,27 @@
 package dev.williamreed.signal
 
-import org.whispersystems.libsignal.SessionCipher
 import org.whispersystems.libsignal.SignalProtocolAddress
 import org.whispersystems.libsignal.state.PreKeyBundle
+import org.whispersystems.libsignal.state.PreKeyRecord
 import org.whispersystems.libsignal.state.SignalProtocolStore
 import org.whispersystems.libsignal.state.impl.InMemorySignalProtocolStore
 import org.whispersystems.libsignal.util.KeyHelper
+import java.util.*
 
 /**
  * Client
  *
  * A client entity with its own keys
  */
-class Client(name: String, deviceId: Int) {
+class Client(name: String, private val deviceId: Int) {
     val store: SignalProtocolStore
     val address = SignalProtocolAddress(name, deviceId)
-    val preKey: PreKeyBundle
+    private val preKeys = ArrayDeque<PreKeyRecord>()
+    private var preKeyId = 0
 
     init {
         this.store = generateIdentity()
-        // not sure about these ids
-        val preKeyId = 0
-        val signedPreKeyId = 0
-        generatePreKeyIdentity(preKeyId, signedPreKeyId)
-
-        val pubicPreKey = store.loadPreKey(preKeyId).keyPair.publicKey
-        val signedPreKey = store.loadSignedPreKey(signedPreKeyId)
-        val publicSignedPreKey = signedPreKey.keyPair.publicKey
-        val identityKey = store.identityKeyPair.publicKey
-        preKey = PreKeyBundle(
-            store.localRegistrationId,
-            deviceId,
-            preKeyId,
-            pubicPreKey,
-            signedPreKeyId,
-            publicSignedPreKey,
-            signedPreKey.signature,
-            identityKey
-        )
+        generatePreKeyIdentity()
     }
 
     /**
@@ -52,16 +36,52 @@ class Client(name: String, deviceId: Int) {
     /**
      * Generate and store pre key
      */
-    private fun generatePreKeyIdentity(preKeyId: Int, signedPreKeyId: Int) {
-        // TODO: store in circular queue and gen more when low
-        val preKeys = KeyHelper.generatePreKeys(preKeyId, 100)
-        val signedPreKey = KeyHelper.generateSignedPreKey(store.identityKeyPair, signedPreKeyId)
+    private fun generatePreKeyIdentity() {
+        val signedPreKey = KeyHelper.generateSignedPreKey(store.identityKeyPair, SIGNED_PRE_KEY_ID)
+        generatePreKeys()
 
+        // store signed keys
+        store.storeSignedPreKey(signedPreKey.id, signedPreKey)
+    }
+
+    /**
+     * Generate pre keys
+     */
+    private fun generatePreKeys() {
+        val preKeys = KeyHelper.generatePreKeys(preKeyId, PRE_KEY_COUNT)
         // store pre keys
         preKeys.forEach {
             store.storePreKey(it.id, it)
+            this.preKeys.push(it)
         }
-        // store signed keys
-        store.storeSignedPreKey(signedPreKey.id, signedPreKey)
+        preKeyId += PRE_KEY_COUNT
+    }
+
+    /**
+     * Get the next pre key bundle to use
+     */
+    fun nextPreKey(): PreKeyBundle {
+        if (preKeys.isEmpty())
+            generatePreKeys()
+
+        val preKey = preKeys.pop()
+        val signedPreKey = store.loadSignedPreKey(SIGNED_PRE_KEY_ID)
+
+        return PreKeyBundle(
+            store.localRegistrationId,
+            deviceId,
+            preKey.id,
+            preKey.keyPair.publicKey,
+            SIGNED_PRE_KEY_ID,
+            signedPreKey.keyPair.publicKey,
+            signedPreKey.signature,
+            store.identityKeyPair.publicKey
+        )
+    }
+
+    companion object {
+        const val PRE_KEY_COUNT = 100
+        // TODO: we only ever need one pre key right?
+        const val SIGNED_PRE_KEY_ID = 123456
     }
 }
--- src/main/kotlin/dev/williamreed/signal/Main.kt
+++ src/main/kotlin/dev/williamreed/signal/Main.kt
@@ -4,8 +4,8 @@ fun main() {
     val alice = Client("alice", 1234)
     val bob = Client("bob", 9876)
 
-    val aliceToBob = Session(alice, bob.address, bob.preKey)
-    val bobToAlice = Session(bob, alice.address, alice.preKey)
+    val aliceToBob = Session(alice, bob.address, bob.nextPreKey())
+    val bobToAlice = Session(bob, alice.address, alice.nextPreKey())
 
     val msg = aliceToBob.encrypt("hi bob")
     println(String(msg.whisperMessage.body))
