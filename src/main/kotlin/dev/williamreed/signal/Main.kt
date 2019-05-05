af386895eb4a8a409101e037ceaf57951896aec8
William Reed <wreed58@gmail.com>
2019-05-05 17:56:15 -0400

    setup basic webserver

--- src/main/kotlin/dev/williamreed/signal/Main.kt
+++ src/main/kotlin/dev/williamreed/signal/Main.kt
@@ -7,19 +7,18 @@ fun main() {
     val bob = Client("bob", 9876)
 
     // server simulation to demo that bob and alice don't need to talk to each other
-    val server = Server()
-    server.register(Json.stringify(SerializablePreKeysBundle.serializer(), alice.nextPreKeysBundle()))
-    server.register(Json.stringify(SerializablePreKeysBundle.serializer(), bob.nextPreKeysBundle()))
+    Server.register(Json.stringify(SerializablePreKeysBundle.serializer(), alice.nextPreKeysBundle()))
+    Server.register(Json.stringify(SerializablePreKeysBundle.serializer(), bob.nextPreKeysBundle()))
 
     val aliceToBob = Session(
         alice,
         bob.address,
-        Json.parse(SerializablePreKeyBundle.serializer(), server.getDeviceBundle(bob.deviceId)).toPreKeyBundle()
+        Json.parse(SerializablePreKeyBundle.serializer(), Server.getDeviceBundle(bob.deviceId)).toPreKeyBundle()
     )
     val bobToAlice = Session(
         bob,
         alice.address,
-        Json.parse(SerializablePreKeyBundle.serializer(), server.getDeviceBundle(alice.deviceId)).toPreKeyBundle()
+        Json.parse(SerializablePreKeyBundle.serializer(), Server.getDeviceBundle(alice.deviceId)).toPreKeyBundle()
     )
 
     val msg = aliceToBob.encrypt("hi bob")
--- src/main/kotlin/dev/williamreed/signal/Server.kt
+++ src/main/kotlin/dev/williamreed/signal/Server.kt
@@ -1,15 +1,23 @@
 package dev.williamreed.signal
 
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonException
+import spark.Spark.get
+import spark.Spark.post
 
 /**
  * Server
  *
  * Simulating the job of a server holding public key information from various clients
  */
-class Server {
+object Server {
     // map of device ids and bundle
     private val devices = mutableMapOf<Int, SerializablePreKeysBundle>()
+    private const val DEVICE_ID = "deviceId"
+
+    init {
+        setupWebServer()
+    }
 
     /**
      * Register a device with the server
@@ -33,13 +41,54 @@ class Server {
         // remove this key
         devices[deviceId] = preKeysBundle.copy(publicPreKeys = preKeysBundle.publicPreKeys.rest())
 
-        return Json.stringify(SerializablePreKeyBundle.serializer(), SerializablePreKeyBundle(
-            preKeysBundle.localRegistrationId,
-            deviceId,
-            nextPreKey,
-            preKeysBundle.signedPublicPreKey,
-            preKeysBundle.signedPreKeySignature,
-            preKeysBundle.identityPublicKey
-        ))
+        return Json.stringify(
+            SerializablePreKeyBundle.serializer(), SerializablePreKeyBundle(
+                preKeysBundle.localRegistrationId,
+                deviceId,
+                nextPreKey,
+                preKeysBundle.signedPublicPreKey,
+                preKeysBundle.signedPreKeySignature,
+                preKeysBundle.identityPublicKey
+            )
+        )
     }
+
+    /**
+     * Setup web server endpoints
+     */
+    private fun setupWebServer() {
+        post("/register") { req, res ->
+            try {
+                register(req.body())
+            } catch (e: JsonException) {
+                res.status(400)
+                e.message
+            }
+        }
+
+        get("/deviceBundle") { req, res ->
+            val deviceId = req.queryParams(DEVICE_ID)
+
+            when {
+                deviceId == null -> {
+                    res.status(400)
+                    "$DEVICE_ID not provided as a query parameter."
+                }
+                deviceId.toIntOrNull() == null -> {
+                    res.status(400)
+                    "$DEVICE_ID must be an integer."
+                }
+                else -> {
+                    res.type("application/json")
+                    try {
+                        getDeviceBundle(deviceId.toInt())
+                    } catch (e: IllegalStateException) {
+                        res.status(400)
+                        e.message
+                    }
+                }
+            }
+        }
+    }
+
 }
